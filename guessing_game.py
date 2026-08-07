14aedfaf24e68ef77badce43093a940a9d312412
OMAR-ALAZIZ <oxo284306@gmail.com>
2026-08-07 20:53:53 +0200

    Add attempt counter

--- guessing_game.py
+++ guessing_game.py
@@ -4,11 +4,13 @@ secret_number = random.randint(1, 10)
 print("=== Number Guessing Game ===\n")
 print("I'm thinking of a number between 1 and 10")
 
+count_try = 0
+
 while True:
     guess = int(input("Enter your guess: "))
-
+    count_try += 1
     if guess == secret_number:
-        print("Congratulations! You guessed correctly.")
+        print(f"Congratulations! You guessed the number in {count_try} attempts.")
         break
     elif guess < secret_number:
         print("Too Low! Try again.")
