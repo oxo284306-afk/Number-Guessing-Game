230a6be552b2f08b2182fb2d148768d91fd39611
OMAR-ALAZIZ <oxo284306@gmail.com>
2026-08-07 15:04:03 +0200

    Add multiple attempts with while loop

--- guessing_game.py
+++ guessing_game.py
@@ -4,10 +4,13 @@ secret_number = random.randint(1, 10)
 print("=== Number Guessing Game ===\n")
 print("I'm thinking of a number between 1 and 10")
 
-guess = int(input("Enter your guess: "))
+while True:
+    guess = int(input("Enter your guess: "))
 
-if guess == secret_number:
-    print("Congratulations! You guessed correctly.")
-else:
-    print(f"❌ Wrong! The correct number was {secret_number}.")
-    
+    if guess == secret_number:
+        print("Congratulations! You guessed correctly.")
+        break
+    elif guess < secret_number:
+        print("Too Low! Try again.")
+    else:
+        print("Too High! Try again.")
